1ae560e11faf42f6cc8efdb0e8bf08bf42dcb0f9
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-08-22 21:29:54 +0200

    Prepare ProGPU 0.1.0-preview.55

--- scripts/progpu-package-list.sh
+++ scripts/progpu-package-list.sh
@@ -16,10 +16,10 @@ progpu_avalonia_package_projects=(
 )
 
 progpu_avalonia_package_versions=(
-  "12.0.5-preview.54"
-  "12.0.5-preview.54"
-  "11.3.18-preview.54"
-  "11.3.18-preview.54"
+  "12.0.5-preview.55"
+  "12.0.5-preview.55"
+  "11.3.18-preview.55"
+  "11.3.18-preview.55"
 )
 
 if [[ "${#progpu_avalonia_package_ids[@]}" -ne "${#progpu_avalonia_package_projects[@]}" ||
