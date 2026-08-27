5e6dec9c6df7abe02d408b30ad0c262761f5b6b2
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-08-27 05:46:06 +0200

    Restore managed drawing metadata contracts

--- src/System.Drawing.Common/System/Drawing/BufferedGraphics.cs
+++ src/System.Drawing.Common/System/Drawing/BufferedGraphics.cs
@@ -3,7 +3,7 @@ using System;
 namespace System.Drawing;
 
 [AttributeUsage(AttributeTargets.Assembly)]
-public sealed class BitmapSuffixInSatelliteAssemblyAttribute : Attribute
+public class BitmapSuffixInSatelliteAssemblyAttribute : Attribute
 {
 }
 
