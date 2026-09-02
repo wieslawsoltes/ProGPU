277b6169122b65790da5a9a604844fb39ae13313
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2026-09-02 08:08:24 +0200

    Collapse SVG corpus source build graph

--- eng/progpu-prepare-svg-system-drawing.sh
+++ eng/progpu-prepare-svg-system-drawing.sh
@@ -19,6 +19,8 @@ cp "$package_overlay" "$svg_source_root/Directory.Packages.props"
 
 target_frameworks_before='<TargetFrameworks>net8.0;net9.0;netcoreapp3.1;netstandard2.1;netstandard2.0;net462;net472;net481</TargetFrameworks>'
 target_frameworks_after='<TargetFrameworks>net10.0;net8.0;net9.0;netcoreapp3.1;netstandard2.1;netstandard2.0;net462;net472;net481</TargetFrameworks>'
+generate_package_before='<GeneratePackageOnBuild>true</GeneratePackageOnBuild>'
+generate_package_after='<GeneratePackageOnBuild>false</GeneratePackageOnBuild>'
 net9_constants='<DefineConstants Condition="$(TargetFramework.StartsWith('\''net9'\''))">$(DefineConstants);NETCORE;NETNEXT</DefineConstants>'
 net10_constants='<DefineConstants Condition="$(TargetFramework.StartsWith('\''net10'\''))">$(DefineConstants);NETCORE;NETNEXT</DefineConstants>'
 progpu_reference='<ProjectReference Condition="'\''$(TargetFramework)'\'' == '\''net10.0'\'' and '\''$(UseProGpuSystemDrawing)'\'' == '\''true'\''" Include="$(ProGpuSourceRoot)/src/System.Drawing.Common/System.Drawing.Common.csproj" AdditionalProperties="ManagePackageVersionsCentrally=true" />'
@@ -26,6 +28,15 @@ progpu_reference='<ProjectReference Condition="'\''$(TargetFramework)'\'' == '\'
 if ! grep -Fq "$target_frameworks_after" "$svg_project"; then
   grep -Fq "$target_frameworks_before" "$svg_project"
   sed -i "s|$target_frameworks_before|$target_frameworks_after|" "$svg_project"
+fi
+
+if ! grep -Fq "$generate_package_after" "$svg_project"; then
+  grep -Fq "$generate_package_before" "$svg_project"
+  sed -i "s|$generate_package_before|$generate_package_after|" "$svg_project"
+fi
+
+if grep -Fq '<PackageReference Include="Nerdbank.GitVersioning"' "$svg_project"; then
+  sed -i '/<PackageReference Include="Nerdbank.GitVersioning"/,/<\/PackageReference>/d' "$svg_project"
 fi
 
 if ! grep -Fq "$net10_constants" "$svg_project"; then
@@ -39,5 +50,7 @@ if ! grep -Fq "$progpu_reference" "$svg_project"; then
 fi
 
 [[ $(grep -Fc "$target_frameworks_after" "$svg_project") -eq 1 ]]
+[[ $(grep -Fc "$generate_package_after" "$svg_project") -eq 1 ]]
 [[ $(grep -Fc "$net10_constants" "$svg_project") -eq 1 ]]
 [[ $(grep -Fc "$progpu_reference" "$svg_project") -eq 1 ]]
+! grep -Fq '<PackageReference Include="Nerdbank.GitVersioning"' "$svg_project"
